37f32a06f92057a0ee154daee112c0c27ca8e584
Wilde Rossi <wilde.rossi@db1.com.br>
2017-12-14 18:47:35 -0200

    Incluindo setter

--- front/app/scripts/15.js
+++ front/app/scripts/15.js
@@ -1,7 +1,22 @@
 const mapInitializer = (() => {
 
 	let map;
-	let coordinates;
+	let coordinates = {
+		lat : undefined,
+		lng : undefined,
+		set latLng(value) {
+			this.lat = value.lat;
+			this.lng = value.lng;
+			showPosition();
+			if (mark) {
+				mark.setMap(null);
+			}
+			markMap();
+		},
+		get isDefined() {
+			return this.lat && this.lng;
+		}
+	};
 	let mark;
 
 	const init = () => {
@@ -25,11 +40,10 @@ const mapInitializer = (() => {
 	};
 
 	const showMarkedMap = (position) => {
-		coordinates = {
+		coordinates.latLng = {
 			lat: position.coords.latitude,
 			lng: position.coords.longitude
 		};
-		showPosition();
 		showMap();
 		markMap()
 		registerClickListener();
@@ -43,13 +57,11 @@ const mapInitializer = (() => {
 
 	const showMap = () => {
 		let mapInitialState = getMapInitialState();
-		return {
-			map: getMap(mapInitialState),
-			point: mapInitialState.center
-		};
+		getMap(mapInitialState);
 	}
 
 	const showPosition = () => {
+		hideElement('geolocation-problem-message');
 		showElement('your-localization');
 		document.getElementById('latitude').innerHTML = coordinates.lat;
 		document.getElementById('longitude').innerHTML = coordinates.lng;
@@ -64,12 +76,16 @@ const mapInitializer = (() => {
 		document.getElementById(elementID).classList.remove('display-none');
 	}
 
+	const hideElement = (elementID) => {
+		document.getElementById(elementID).classList.add('display-none');
+	};
+
 	const getMapInitialState = () => {
 		let mapInitialState = {
 			zoom: 3,
 			center: { lat: 0, lng: 0 }
 		};
-		if (coordinates !== undefined) {
+		if (coordinates.isDefined) {
 			mapInitialState = {
 				zoom: 6,
 				center: coordinates
@@ -95,7 +111,7 @@ const mapInitializer = (() => {
 	};
 
 	const onMapDblClick = (e) => {
-		coordinates = {
+		coordinates.latLng = {
 			lat: e.latLng.lat(),
 			lng: e.latLng.lng()
 		}
